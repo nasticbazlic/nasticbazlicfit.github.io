982e151bc9d17e8e573b460afb1293f0922e8d5c
Nastya <nasticbazlic@gmail.com>
2020-11-25 23:26:57 -0900

    validate forms

--- src/js/script.js
+++ src/js/script.js
@@ -93,4 +93,37 @@ $(function() {
 			$('.overlay, #order').fadeIn('slow');
 		});
 	});
+
+	
+
+	function validateForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				  },
+				phone:"required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name:  {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите более {0} симолов")
+				  },
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+				  required: "Пожалуйста, введите свою почту",
+				  email: "Ваш адрес электронной почты должен быть в формате name@domain.com."
+				}
+			  }
+		});
+	};
+	validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+	
 });
