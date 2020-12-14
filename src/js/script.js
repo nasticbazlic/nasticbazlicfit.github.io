c4822152893bdb9bc1cf4e70a81bd2d171b7ef12
Nastya <nasticbazlic@gmail.com>
2020-12-13 22:52:16 -0900

    script validation

--- src/js/script.js
+++ src/js/script.js
@@ -152,7 +152,7 @@ $(function() {
 			$('.pageup').fadeOut();
 		}
 	});
-	$("a[href^='#']").click(function(){
+	$("a[href^=#up]").click(function(){
 		const _href = $(this).attr("href");
 		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 		return false;
